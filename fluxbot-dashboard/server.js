03daad2969fde55857e3f2c3800fdf7f656b1688
0xLeoFX <papicakes8@gmail.com>
2025-08-26 02:47:10 +0000

    Added real-time updates

--- fluxbot-dashboard/server.js
+++ fluxbot-dashboard/server.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const { MongoClient } = require('mongodb');
-require('dotenv').config({ quiet: true });
+require('dotenv').config();
+const http = require('http');
+const { Server } = require('socket.io');
 
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
 const MONGO_URI = process.env.MONGO_URI;
 
 async function connectMongo() {
@@ -16,11 +20,24 @@ async function connectMongo() {
 }
 
 app.use(express.static('public'));
+app.use(express.json());
 
 app.get('/leads', async (req, res) => {
   const db = await connectMongo();
   const leads = await db.collection('leads').find().toArray();
-  res.json(leads);
+  const consents = await db.collection('consents').find().toArray();
+  res.json({ leads, consents });
 });
 
-app.listen(3000, () => console.log('Dashboard running on port 3000'));
+app.post('/process-lead', async (req, res) => {
+  const db = await connectMongo();
+  await db.collection('leads').updateOne({ _id: req.body._id }, { $set: { processed: true } });
+  io.emit('leadUpdated');
+  res.send('Lead processed');
+});
+
+io.on('connection', (socket) => {
+  console.log('Client connected');
+});
+
+server.listen(3000, () => console.log('Dashboard running on port 3000'));
